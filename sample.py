61950018c0a668d90d9e7788e50ceb30f9ce5ce9
Nicolas Dubois <nicolas.c.dubois@gmail.com>
2013-06-29 23:29:03 +0200

    Added ≤, ≥

--- sample.py
+++ sample.py
@@ -1,16 +1,27 @@
+
+12 ** 1
 12 ** 2
-12 ** 3
-12 ** 4
 12**2
 12   **  2
+12 ** 3
+12 ** 4
 
-foo = bar
 foo = bar
 foo=bar
+
 foo == bar
 foo ==bar
+foo =="bar"
+"foo" =="bar"
 
 foo != "bar"
+"foo"!="bar"
 
-foo // bar
 foo / bar
+foo // bar
+
+foo >= bar
+foo <= bar
+
+sqrt(x)
+sqrt(25)
